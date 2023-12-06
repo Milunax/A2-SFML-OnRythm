45fc0eff5076c1fdc6b43f098a47c462577d5e80
Clément THOMAS <clemto95@live.fr>
2023-12-06 16:49:20 +0100

    DEBUG HealthBar

--- HealthBar.cpp
+++ HealthBar.cpp
@@ -5,7 +5,7 @@ HealthBar::HealthBar(sf::Vector2f startPos, sf::Color color, sf::Vector2f size) 
 }
 
 void HealthBar::UpdateSize(int value, int maxValue) {
-	_size.x = (float)(value / maxValue) * _sizeMax.x;
+	_size.x = ((float)value / maxValue) * _sizeMax.x;
 	std::cout << _size.x << std::endl;
 	_size.x = std::clamp(_size.x, 0.0f, _sizeMax.x);
 }
