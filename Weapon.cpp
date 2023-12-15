013ef680ced834d1a5ccbd5f2bf80851185d4b9a
Clément THOMAS <clemto95@live.fr>
2023-12-15 13:32:54 +0100

    ADD Base Weapon class

--- Weapon.cpp
+++ Weapon.cpp
@@ -1,1 +1,27 @@
 #include "Weapon.h"
+
+Weapon::Weapon(float damages, float attackRate)
+{
+	_damages = damages;
+	_fireTimer = 0;
+	_attackRate = attackRate;
+}
+
+void Weapon::Init(Player* player)
+{
+	_player = player;
+}
+
+void Weapon::Draw(RefsData data)
+{
+}
+
+void Weapon::Update()
+{
+	_position = _player->GetPosition();
+	_orientationDirection = _player->GetOrientationDirection();
+}
+
+void Weapon::Attack()
+{
+}
--- Weapon.h
+++ Weapon.h
@@ -1,9 +1,21 @@
 #pragma once
+#include <SFML/System/Vector2.hpp>
+#include "RefsData.h"
+#include "Player.h"
 
 class Weapon {
     public:
-        void Draw();
+        Weapon(float damages, float attackRate);
+        virtual void Init(Player* player);
+        virtual void Draw(RefsData data);
+        virtual void Update();
+        virtual void Attack();
     protected:
-        float _fireRate;
+        float _fireTimer;
+        float _attackRate;
         float _damages;
+        sf::Vector2f _position;
+        sf::Vector2f _orientationDirection;
+
+        Player* _player;
 };
